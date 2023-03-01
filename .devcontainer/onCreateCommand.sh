617ad194e51ba0c62c3a727327999aa79f863458
Anders Starcke Henriksen <starcke@github.com>
2023-03-01 15:03:31 +0100

    Apply suggestions from code review
    Co-authored-by: Elena Tanasoiu <elenatanasoiu@github.com>

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -2,5 +2,6 @@
 
 # Assumes the gh CLI is present in the default Codespaces image.
 gh extensions install github/gh-codeql
-gh codeql version # first command starts the download
+gh codeql version # first command starts the download of the CodeQL CLI
+# Make codeql visible to VSCode by using https://github.com/github/gh-codeql#codeql-stub, since VS Code expects an executable called codeql instead of gh codeql
 gh codeql install-stub ~/.local/bin/
