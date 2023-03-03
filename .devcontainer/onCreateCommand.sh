6fca790ad73455c6bba8da19f5fcecd7ffc60cd0
shati-patel <42641846+shati-patel@users.noreply.github.com>
2023-03-03 14:32:13 +0000

    Install tutorial pack dependencies in codespace prebuild

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -5,3 +5,9 @@ gh extensions install github/gh-codeql
 gh codeql version # first command starts the download of the CodeQL CLI
 # Make codeql visible to VSCode by using https://github.com/github/gh-codeql#codeql-stub, since VS Code expects an executable called codeql instead of gh codeql
 gh codeql install-stub ~/.local/bin/
+
+# Install the tutorial-queries pack
+cd tutorial-queries
+echo "Installing tutorial-queries pack"
+gh codeql pack install
+cd ..
