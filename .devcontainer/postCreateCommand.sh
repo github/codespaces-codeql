9f832e653436952a7a563fc565fff31d12420b24
shati-patel <42641846+shati-patel@users.noreply.github.com>
2023-03-17 16:35:38 +0000

    Remove unnecessary `postCreateCommand`
    We no longer need to copy these files over as of https://github.com/github/codespaces-codeql/pull/6.

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Copy the dbscheme into the tutorial library, so it matches the DB.
-cp .tours/codeql-tutorial-database/db-csv/csv.dbscheme tutorial-queries/
-cp .tours/codeql-tutorial-database/db-csv/csv.dbscheme.stats tutorial-queries/
